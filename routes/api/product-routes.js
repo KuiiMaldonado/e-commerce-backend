5e06061e934af471d9c32039f9814d7b4b43d774
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-06-07 17:15:21 -0500

    api/products/id route returns product with the specified id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -18,9 +18,22 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [{model: Category}, {model: Tag}]
+    });
+    if (product === null) {
+      res.status(404).json({message: 'Product not found'});
+    }
+    else {
+      res.status(200).json(product);
+    }
+  }catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
