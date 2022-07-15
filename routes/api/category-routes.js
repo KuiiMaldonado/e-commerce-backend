0c1ac4ae0b303b3adb7874d48dfc07d732165764
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-07-15 14:08:28 -0500

    Update category route working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -29,12 +29,28 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+    try {
+        const newCategory = await Category.create(req.body)
+        res.status(200).json(newCategory);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+    try {
+        const updatedCategory = await Category.update(req.body, {
+            where: {
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(updatedCategory);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 router.delete('/:id', (req, res) => {
