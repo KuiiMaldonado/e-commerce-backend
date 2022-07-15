30ffc36bcd4022b116e1600a0cddfd04c58ba8ca
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-07-15 14:14:38 -0500

    Delete category by id working

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -47,14 +47,30 @@ router.put('/:id', async (req, res) => {
                 id: req.params.id,
             },
         });
-        res.status(200).json(updatedCategory);
+        if (updatedCategory !== 0)
+            res.status(200).json({message: "Category updated"});
+        else
+            res.status(400).json({message: "No category with the id provided"});
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+    try {
+        const isDeleted = await Category.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (isDeleted !== 0)
+            res.status(200).json({message: "Category deleted"});
+        else
+            res.status(400).json({message: "No category with the id provided"})
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
